5723f4f453f568bd5075c41f274f06bd178150bb
Batis <abba713@gmail.com>
2021-09-21 12:07:41 +0430

    Complete Some part of the Codes

--- truffle.js
+++ truffle.js
@@ -96,7 +96,8 @@
        //  evmVersion: "byzantium"
      }
    }
- };var HDWalletProvider = require("truffle-hdwallet-provider");
+ };
+// var HDWalletProvider = require("truffle-hdwallet-provider");
 var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble sweet treat";
 
 module.exports = {
